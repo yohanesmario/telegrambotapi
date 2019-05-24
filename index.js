dec852548f4bb5be2580f86c48b4a1ce1308e5b9
Yohanes Mario Chandra <yohanes.mc@gmail.com>
2019-05-24 13:04:11 +0700

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const Telegram = require('telegram-bot-api');
+
+const api = new Telegram({
+  token: '[YOUR_BOT_API_TOKEN]', //Fill your bot API token here. 
+  updates: {
+    enabled: true //This one is to make sure the library is requesting for updates, or else, your program will exit
+  }
+});
+
+//Register handler on update
+api.on('update', function(notif) {
+  //We're just gonna log the notif first
+  console.log(notif);
+});
